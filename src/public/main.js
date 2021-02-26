26acdc05d123a45f5ae84bc27ecf587fb293ad09
citizen-dror <reshef.dror@gmail.com>
2021-02-26 19:45:16 +0200

    add basic canvas

--- src/public/main.js
+++ src/public/main.js
@@ -1,5 +1,17 @@
 import IslandCounter from './IslandCounter';
 
+const draw = () => {
+  const canvas = document.getElementById('islandsCanavas');
+  if (canvas.getContext) {
+    const ctx = canvas.getContext('2d');
+    ctx.fillStyle = 'rgb(200, 0, 0)';
+    ctx.fillRect(10, 10, 50, 50);
+
+    ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+    ctx.fillRect(30, 30, 50, 50);
+  }
+};
+
 const runCount = () => {
   console.log('start count');
   const start = new Date().getTime();
@@ -10,6 +22,7 @@ const runCount = () => {
   const timeDiff = new Date().getTime() - start;
   console.log('count: ', count);
   console.log('time:', timeDiff);
+  draw();
 };
 
 document.getElementById('btnCount').addEventListener('click', runCount, false);
